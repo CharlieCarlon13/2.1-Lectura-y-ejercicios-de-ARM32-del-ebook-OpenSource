71b3a622b610c87d0b08de0a974ecfb7dd8403c9
Fernando Oliva <71294422+FernandoOliva18212205@users.noreply.github.com>
2020-10-23 20:28:54 -0700

    Add files via upload

---
+++ Programas/intro2.s
@@ -0,0 +1,24 @@
+    @@
+    @@ Instituto Tecnologico de Tijuana
+    @@ Depto de Sistemas y Computación
+    @@ Ing. En Sistemas Computacionales
+    @@   
+    @@ Autor : Carlos Fernando Leal Oliva
+    @@ Fecha de elaboracion: 23/10/2020
+    @@ 
+
+.data
+
+var1 : .byte 0b00110010
+	.align
+var2 : .byte 0b11000000
+	.align
+.text
+.global main
+
+main:	ldr 	r1, = var1 	/* r1 <- & var1 */
+	ldrsb 	r1, [ r1 ] 	/* r1 <- *r1 */
+	ldr 	r2, = var2 	/* r2 <- & var2 */
+	ldrsb 	r2, [ r2 ] 	/* r2 <- *r2 */
+	add 	r0, r1, r2 	/* r0 <- r1 + r2 */
+	bx 	lr
